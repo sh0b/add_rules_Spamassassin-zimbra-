31194a24942c2f32cdf1590ecfb40609f4fec4ad
Sebastian Batalla <sbbt.uy@gmail.com>
2013-08-09 12:15:41 -0300

    args usages

--- psalocal.py
+++ psalocal.py
@@ -2,17 +2,15 @@
 #
 # MOD salocal.in of {zimbra}
 #
+__author__ = 'SebastianBatalla'
+
 
 import re
 import sys
 import shutil
 import datetime
 import os
-
-
-## Backup
-today = str(datetime.date.today())
-shutil.copy2('salocal.cf.in', 'salocal.in.'+today)
+import argparse
 
 input = open('salocal.cf.in','r+')
 output = open('salocal.cf.in.tmp','w')
@@ -29,10 +27,9 @@ def valid_email(email):
         return False
     return re.match(r"^[a-zA-Z0-9._%-+]+\@[a-zA-Z0-9._%-]+\.[a-zA-Z]{2,}$", email)!=None
 
-def add_spam_words(meta,body,fmeta):
+def add_spam_words(meta,body,fmeta,rule_sa):
 	match_count=count_match(body)
 	input.seek(0)
-	rule_sa=raw_input('Rule: ')
 	pattern = re.compile(meta)
 
 	for line in input:	
@@ -43,8 +40,7 @@ def add_spam_words(meta,body,fmeta):
 			output.write(line)
 	print "-- Rule added -- \n"
 
-def add_to_blacklist():
-	email_bl=raw_input('Blacklist to: ') 
+def add_to_blacklist(email_bl):
 	if not valid_email(email_bl):
 		print "\nNo es un e-mail valido"
 		sys.exit()
@@ -55,22 +51,34 @@ def add_to_blacklist():
 		output.write ("\nblacklist_to "+ email_bl+"\n")
 		
 		print "-- Blacklist added -- \n"
+
+
+def main(n):
 	
-### Menu ###
+	parser = argparse.ArgumentParser(description='Admin salocal Zimbra.')
+	parser.add_argument('-o','--option', help='\n Agregar a Spam  0 - Palabra de spam (Nivel normal) 1 - Palabra de spam (Nivel alto)  2 - Agregar cuenta a blacklist  3 - Salir',required=True)
+	parser.add_argument('-p','--param',help='Palabra de spam / e-mail', required=True)
+	n = parser.parse_args()
+	
+	## Backup
+	today = str(datetime.date.today())
+	shutil.copy2('salocal.cf.in', 'salocal.in.'+today)
 
-n=raw_input("\n Agregar a Spam \n 0 - Palabra de spam (Nivel normal) \n 1 - Palabra de spam (Nivel alto) \n 2 - Agregar cuenta a blacklist \n 3 - Salir \n")
+	### Menu ###
 
+	if n.option=="0":
+		add_spam_words("meta SPAM_WORDS","body __WORDS","__WORDS",n.param)
+	elif n.option=="1":
+		add_spam_words("meta SPAM_AGGRESSIVE_WORDS","body __AGGWORDS","__AGGWORDS",n.param)
+	elif n.option=="2":
+		add_to_blacklist(n.param);
+	else:
+		sys.exit();
 
-if n=='0':
-	add_spam_words("meta SPAM_WORDS","body __WORDS","__WORDS");
-elif n=='1':	
-	add_spam_words("meta SPAM_AGGRESSIVE_WORDS","body __AGGWORDS","__AGGWORDS");
-elif n=='2':
-	add_to_blacklist();
-else:
-	sys.exit();
+	input.close()
+	output.close()
 
-input.close()
-output.close()
+	shutil.copy2('salocal.cf.in.tmp','salocal.cf.in')
 
-shutil.copy2('salocal.cf.in.tmp','salocal.cf.in')
+if __name__ == '__main__':
+	main(sys.argv)
